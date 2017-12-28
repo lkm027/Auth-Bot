d907c75b1c6927fecaf233fd38c377af8905d1e6
lucasm <lkm027@gmail.com>
2017-12-28 14:37:36 -0500

    Split up code to into different modules; added new get_members module

---
+++ get_group_members.py
@@ -0,0 +1,12 @@
+import requests
+import json
+import os
+
+headers = {
+        "X-Access-Token" : os.getenv( "USER_ID" ),
+        "Content-type"   : "application/json"
+        }
+
+group_id = os.getenv( "GROUPME_GROUP_ID" )
+
+request = requests.post( "https://api.groupme.com/v3/groups/" + group_id, data = json.dumps( {} ), headers = headers )
---
+++ send_message.py
@@ -0,0 +1,12 @@
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+def send_groupme_message( msg ):
+    url = 'https://api.groupme.com/v3/bots/post'
+
+    data = {
+            'bot_id' : os.getenv( 'GROUPME_BOT_ID'),
+            'text'   : msg,
+            }
+    request = Request( url, urlencode( data ).encode() )
+    json = urlopen( request ).read().decode()
