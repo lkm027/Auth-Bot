2ba563cb572b28ca3f2612b6967ad4e57d277786
lucasm <lkm027@gmail.com>
2017-12-29 18:59:58 -0500

    Added a lot of code to create and insert into a members table

--- get_group_members.py
+++ get_group_members.py
@@ -6,7 +6,15 @@ import psycopg2
 from send_message import send_groupme_message
 from urllib import parse
 
-def get_members():
+def check_and_add_members_if_none_exist():
+    if( check_if_member_table_exists() == False ):
+        create_members_table()
+    if( is_group_members_table_empty() == True ):
+        get_members_and_add_to_table()
+    return True
+
+
+def get_members_and_add_to_table():
     headers = {
             "X-Access-Token" : os.getenv( "USER_ID" ),
             "Content-type"   : "application/json"
@@ -24,6 +32,51 @@ def get_members():
 
     for member in members:
         send_groupme_message( member['nickname'] )
+        try:
+            parse.uses_netloc.append("postgres")
+            url = parse.urlparse(os.environ["DATABASE_URL"])
+
+            conn = psycopg2.connect(
+                database=url.path[1:],
+                user=url.username,
+                password=url.password,
+                host=url.hostname,
+                port=url.port
+            )
+
+            cursor = conn.cursor()
+            cursor.execute( "INSERT INTO members VALUES ( " + member['user_id'] + ", False );" )
+            cursor.close()
+            conn.close()
+
+    except Exception as e:
+        print( "Could add new members to members table." )
+        print( e )
+
+def create_members_table():
+    try:
+        parse.uses_netloc.append("postgres")
+        url = parse.urlparse(os.environ["DATABASE_URL"])
+
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+
+        cursor = conn.cursor()
+        cursor.execute( """CREATE TABLE members 
+                            ( id SERIAL PRIMARY KEY, 
+                              name VARCHAR(80),
+                              isAdmin BOOLEAN );""" )
+        cursor.close()
+        conn.close()
+
+    except Exception as e:
+        print( "Could not create new members table." )
+        print( e )
 
 def is_group_members_table_empty():
     try:
@@ -45,9 +98,9 @@ def is_group_members_table_empty():
         conn.close()
 
         if( rows[0][0] == 0 ):
-            return false
+            return False
 
-        return true
+        return True
 
     except Exception as e:
         print( "Connecting to db failed for some reason" )
@@ -75,6 +128,10 @@ def check_if_member_table_exists():
         cursor.close()
         conn.close()
 
-        print( rows[0] )
+        if( rows[0][0] == False )
+            return False
+
+        return True
+
     except Exception as e:
         print( "Connecting went wrong somewhere" )
