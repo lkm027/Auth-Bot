a95cb2c3fd7703f3d0dff321a30f1a42b85f4e70
lucasm <lkm027@gmail.com>
2017-12-29 23:52:27 -0500

    Debugging calling auth bot

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ def webhook():
     # We don't want to reply do ourselves!
     if( data['name'] != os.getenv( "BOT_NAME" ) ):
         words = str.split( data["text"] )
+        print( words[0] )
+        print( words[1] )
         if( words[0].lower() == "@Auth" and words[1].lower() == "Bot" ):
             check_all_commands( data["text"] )
 
