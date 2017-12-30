19a8367390cf8d5c10c459a31f0701d9d1b782cf
lucasm <lkm027@gmail.com>
2017-12-30 17:20:30 -0500

    Debugging changing name

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ app = Flask(__name__)
 def webhook():
     data = request.get_json()
 
-    print( data["text"] )
-    print( data["name"] )
-
     # We don't want to reply do ourselves!
     if( data['name'] != os.getenv( "BOT_NAME" ) ):
         words = str.split( data["text"] )
@@ -22,17 +19,16 @@ def webhook():
             check_all_commands( data["text"] )
 
     if( data['name'] == "GroupMe" ):
-        print( "made it here" )
         phrase = data["text"]
         if( "changed name to" in phrase ):
-            print( "In it" )
             words = str.split( phrase )
             last_index = 0
             for index, word in enumerate( words ):
                 if( word == "changed" ):
                     last_index = index - 1
                     break
-            name_before_change = " ".join( words[:, last_index] )
+            print( index )
+            name_before_change = " ".join( words[:last_index] )
             print( name_before_change )
 
 
