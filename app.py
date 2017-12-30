2a76c989a9f478e9e2244f95765060f320d5a185
lucasm <lkm027@gmail.com>
2017-12-30 16:53:32 -0500

    Added test to get user name when they change their nickname

--- app.py
+++ app.py
@@ -11,13 +11,23 @@ app = Flask(__name__)
 @app.route( '/', methods=['POST'] )
 def webhook():
     data = request.get_json()
-    print( data['name'] )
-    print( data['text'] )
 
     # We don't want to reply do ourselves!
     if( data['name'] != os.getenv( "BOT_NAME" ) ):
         words = str.split( data["text"] )
         if( words[0].lower() == "@auth" and words[1].lower() == "bot" ):
             check_all_commands( data["text"] )
+    else if ( data['name'] = "GroupMe" ):
+        phrase = data["text"]
+        if( "changed name to" in phrase ):
+            words = str.split( phrase )
+            last_index = 0
+            for index, word in enumerate( words ):
+                if( word == "changed" ):
+                    last_index = index - 1
+                    break
+            name_before_change = " ".join( words[:, last_index] )
+            print( name_before_change )
+
 
     return "ok", 200
