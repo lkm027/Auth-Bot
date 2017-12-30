6c3e16c89c66082680f0140573026c5463cd2c44
lucasm <lkm027@gmail.com>
2017-12-30 00:00:54 -0500

    Remove the @auth bot call from a user command request

--- commands.py
+++ commands.py
@@ -2,6 +2,12 @@ from send_message import send_groupme_message
 from group_members import check_and_add_members_if_none_exist
 
 def check_all_commands( command ):
+    # Remove the auth bot call from our command
+    words = str.split( command )
+    words.pop( 0 )
+    words.pop( 0 )
+    command = "".join( words )
+
     command.lower()
     print( command )
     return {
