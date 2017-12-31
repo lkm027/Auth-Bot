cd8e86f533108e120ed459dd874371d0494fe9ae
lucasm <lkm027@gmail.com>
2017-12-31 15:07:50 -0500

    check if the member who posted is an admin

---
+++ member_post.py
@@ -0,0 +1,32 @@
+import os
+import psycopg2
+
+from urllib import parse
+
+def check_if_member_is_admin( member ):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM tb_members WHERE nickname='" + member + "';" )
+    cursor.fetchall()
+    print( cursor[0] )
+    cursor.close()
+    conn.close()
+
+def get_db_connection():
+    try:
+        parse.uses_netloc.append("postgres")
+        url = parse.urlparse(os.environ["DATABASE_URL"])
+
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+
+        return conn
+
+    except Exception as e:
+        print( "Could not establish a connection with the database." )
+        print( e )
