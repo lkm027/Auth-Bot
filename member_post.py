b8b831c85249ac3720f88a0a53ed37e3082a6986
lucasm <lkm027@gmail.com>
2017-12-31 15:48:07 -0500

    add warnings when users that are not admins post in the group

--- member_post.py
+++ member_post.py
@@ -3,13 +3,29 @@ import psycopg2
 
 from urllib import parse
 
-def check_if_member_is_admin( member ):
+from send_message import send_groupme_message
+
+def check_if_member_is_admin( member_id ):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute( "SELECT * FROM tb_members WHERE nickname='" + member + "';" )
+    cursor.execute( "SELECT * FROM tb_members WHERE user_id='" + member_id + "';" )
     rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
     if( not rows[0][3] ):
-        print( "User is not an admin" )
+        return False
+    return True
+
+def add_warning_to_member( member_id, member_name ):
+    conn = get_db_connection()
+    curosr = conn.cursor()
+    cursor.execute( "SELECT * FROM tb_members WHERE user_id='" + member_id + "';" )
+    rows = cursor.fetchall()
+    warnings_count = rows[0][5] + 1
+    cursor.execute( "UPDATE tb_members set warnings=" + warnings_count + " where user_id='" + member_id + "';" )
+
+    send_groupme_message( member_name + ", you will be kicked on your 3rd warning. Please do not post again. Your current warning count: " + warnings_count + "." )
+    conn.commit()
     cursor.close()
     conn.close()
 
