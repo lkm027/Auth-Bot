afeca08bc60390843977551c544e09d277e48a30
lucasm <lkm027@gmail.com>
2018-01-01 23:46:02 -0500

    Fix conditional syntax

--- change_name.py
+++ change_name.py
@@ -4,5 +4,5 @@ from db_connection import get_db_connection
 from db_requests import check_if_member_exists_by_name, change_member_name
 
 def change_name_if_it_exists( name_before, name_after ):
-    if( check_if_member_exists_by_name( name_before ):
+    if( check_if_member_exists_by_name( name_before ) ):
         change_member_name( name_before, name_after )
